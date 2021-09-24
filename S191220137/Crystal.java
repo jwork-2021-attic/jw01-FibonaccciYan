2227cce92aed251d118c86798cf6e9f3a0002deb
FibonaccciYan <1363650301@qq.com>
2021-09-24 18:45:23 +0800

    JW01 Finish

---
+++ S191220137/Crystal.java
@@ -0,0 +1,13 @@
+package S191220137;
+
+public class Crystal extends Creature {
+    Crystal(int hp, int ap, int dp) {
+        health = hp;
+        attack = ap;
+        defense = dp;
+    }
+
+    public void beBroken() {
+        System.out.println("I'm broken into peaces");
+    }
+}
